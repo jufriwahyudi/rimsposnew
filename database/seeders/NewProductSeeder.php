68e7130d5d6bbd8ca69c4b620f6e6f8e18a8aaf2
jufriwahyudi <58408344+jufriwahyudi@users.noreply.github.com>
2026-05-19 22:22:02 +0700

    bug fix

--- database/seeders/NewProductSeeder.php
+++ database/seeders/NewProductSeeder.php
@@ -1418,9 +1418,7 @@ class NewProductSeeder extends Seeder
 
             $len_variant = count($p['variants']);
             $nama_produk = $p['nama_produk'];
-            if ($len_variant == 1) {
-                $nama_produk .= ($p['variants'][0]['label'] === 'Default' ? '' : ' - ' . $p['variants'][0]['label']);
-            }
+
             $product = Product::create([
                 'store_id'    => 1,
                 'kode_produk' => $kode,
